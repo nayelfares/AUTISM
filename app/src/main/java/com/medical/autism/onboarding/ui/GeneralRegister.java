6694c47425d24c81324c48e861fc8a55ced7b911
nayelfares <nayel.fares@hotmail.com>
2021-03-23 22:53:20 +0400

    Trainer Registration

--- app/src/main/java/com/medical/autism/onboarding/ui/GeneralRegister.java
+++ app/src/main/java/com/medical/autism/onboarding/ui/GeneralRegister.java
@@ -1,12 +1,49 @@
 package com.medical.autism.onboarding.ui;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.medical.autism.BaseFragment;
 import com.medical.autism.R;
+import com.medical.autism.onboarding.OnboardingProcess;
 
 
 public class GeneralRegister extends BaseFragment {
 
+    EditText username;
+    EditText email;
+    EditText password;
+    EditText mobileNumber;
+    RadioButton trainer;
+    RadioButton pateint;
+    Button next;
     public GeneralRegister() { super(R.layout.fragment_general_register);}
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        username     = requireActivity().findViewById(R.id.generalRegisterUsername);
+        email        = requireActivity().findViewById(R.id.generalRegisterEmail);
+        password     = requireActivity().findViewById(R.id.generalRegisterPassword);
+        mobileNumber = requireActivity().findViewById(R.id.generalRegisterMobileNumber);
+        trainer      = requireActivity().findViewById(R.id.generalRegisterTrainer);
+        pateint      = requireActivity().findViewById(R.id.generalRegisterPatient);
+        next         = requireActivity().findViewById(R.id.generalRegisterNext);
+        next.setOnClickListener(v ->{
+            if (trainer.isChecked()) {
+                ((OnboardingProcess) requireActivity()).replaceFragment(new TrainerRegistration(
+                        username.getText().toString(),
+                        email.getText().toString(),
+                        password.getText().toString(),
+                        mobileNumber.getText().toString()
+                ));
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/medical/autism/onboarding/ui/TrainerRegistration.java
@@ -0,0 +1,17 @@
+package com.medical.autism.onboarding.ui;
+
+
+import com.medical.autism.BaseFragment;
+import com.medical.autism.R;
+
+public class TrainerRegistration extends BaseFragment {
+
+
+
+    public TrainerRegistration(String username,String email,String password,String mobileNumber) {
+        super(R.layout.fragment_trainer_registration);
+    }
+
+
+
+}
