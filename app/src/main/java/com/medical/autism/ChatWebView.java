bdb50408e12325e799dd96c33e78ec4433073f6a
nayelfares <nayel.fares@shaadoow.com>
2021-03-29 14:13:12 +0400

    webView

---
+++ app/src/main/java/com/medical/autism/ChatWebView.java
@@ -0,0 +1,40 @@
+package com.medical.autism;
+
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+public class ChatWebView extends Fragment {
+    WebView webView;
+    String link;
+    public ChatWebView(String link) {
+        super(R.layout.fragment_chat_web_view);
+        this.link= link;
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        webView  = requireActivity().findViewById(R.id.webView);
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically( true);
+        webView.getSettings().setDomStorageEnabled( true);
+        webView.setWebChromeClient(new WebChromeClient());
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        webView.loadUrl(link);
+    }
+}
